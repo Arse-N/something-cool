eaf124df206e6298dad000c5a6746c8702c49ddf
Arse-N <Arsen.karapetyan33@gmail.com>
2023-08-09 23:02:38 +0400

    Initial commit

---
+++ src/main/java/com/just/somethingcool/SomethingCoolApplication.java
@@ -0,0 +1,13 @@
+package com.just.somethingcool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomethingCoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomethingCoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/just/somethingcool/SomethingCoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.just.somethingcool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomethingCoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
